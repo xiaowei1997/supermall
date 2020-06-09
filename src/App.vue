ce14bc2adb3f53c1d4462d4beb6be236d0fd8544
xiaowei1997 <1058006580@qq.com>
2020-06-09 20:11:53 +0800

    添加tabbar和路由

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <template>
   <div id="app">
-  
+    <router-view/>
+    <MainTabBar/>
   </div>
 </template>
 
 <script>
 
+import MainTabBar from "./components/content/mainTabbar/MainTabBar";
 
 export default {
   name: 'App',
   components: {
-  
+    MainTabBar
   }
 }
 </script>
 
 <style>
-
+  @import "assets/css/base.css";
 </style>
